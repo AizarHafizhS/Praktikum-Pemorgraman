4d1a76432527ee82e59d82ebe89f174b0bbd9d19
AizarHafizhS <110295915+AizarHafizhS@users.noreply.github.com>
2024-09-03 14:54:51 +0700

    up folder

---
+++ try_assignment/chapter_2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	int a = 5;
+	int b = 10;
+	string name;
+	
+	
+	int hasil = a+b;
+	getline(cin, name);
+	cout << hasil << endl << name;
+	
+}
